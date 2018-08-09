a33493940d22ecee648bb755e49760c2fa2e6e3d
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-09 15:08:57 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,16 +1,16 @@
 #code your solutions here
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
-#  - almond flour - 2 cups
-#  - gluten freewhole oats - 1 cup
-#  - kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup
+  - almond flour - 2 cups
+  - gluten freewhole oats - 1 cup
+  - kosher salt - 1/2 tea spoon
+  - baking powder  - 1/2 tea spoon
+  - baking soda - 1/4 tea spoon
+  - Xanthan Gum - 1/2 tea spoon
+  - slivered almonds, lightly toasted - 1/4 cup
+  - mini dark chocolate chips - 3/4 cup
+  - olive oil - 1/2 cup
+  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 perfect_10_recipe = {

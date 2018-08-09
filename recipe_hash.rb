0fc1d703aa14a9d6e1471f16e09893e7e347d468
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-09 15:19:13 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -15,7 +15,7 @@
 
 perfect_10_recipe = {
   :almond_flour => "2 cups",
-  :gluten_freewhole oats => "1 cup",
+  :gluten_freewhole_oats => "1 cup",
   :kosher_salt=> "1/2 tea spoon",
   :baking_powder => "1/2 tea spoon",
   :baking_soda => "1/4 teaspoon",
